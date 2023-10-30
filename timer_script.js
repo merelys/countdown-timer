91e8892a78e4192273b0da00f4e2ad46f5610427
merelys <ev.merelys@gmail.com>
2023-10-30 14:16:30 +0100

    english version

--- timer_script.js
+++ timer_script.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
 	
 	const timerItems = $(".timer").children();	
 	
-	function declensionNum(num, words) {
-		return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
-	}
+	// function declensionNum(num, words) {
+		// return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+	// }
 
 	function countdownTimer() {
 			
@@ -18,7 +18,7 @@ $(document).ready(function() {
 		if (diff < 0) {
 			
 			clearInterval(timeId);
-			$(".footer")[0].textContent = "Время вышло!";
+			$(".footer")[0].textContent = "Time is over!";
 			return;
 				
 		}
@@ -28,10 +28,15 @@ $(document).ready(function() {
 		const minutes = Math.floor(diff / 1000 / 60) % 60;
 		const seconds = Math.floor(diff / 1000) % 60;
 			
-		updateDigits(timerItems[0], days, declensionNum(days, ['день', 'дня', 'дней']));
-		updateDigits(timerItems[1], hours, declensionNum(hours, ['час', 'часа', 'часов']));
-		updateDigits(timerItems[2], minutes, declensionNum(minutes, ['минута', 'минуты', 'минут']));
-		updateDigits(timerItems[3], seconds, declensionNum(seconds, ['секунда', 'секунды', 'секунд']));
+		// updateDigits(timerItems[0], days, declensionNum(days, ['день', 'дня', 'дней']));
+		// updateDigits(timerItems[1], hours, declensionNum(hours, ['час', 'часа', 'часов']));
+		// updateDigits(timerItems[2], minutes, declensionNum(minutes, ['минута', 'минуты', 'минут']));
+		// updateDigits(timerItems[3], seconds, declensionNum(seconds, ['секунда', 'секунды', 'секунд']));
+		
+		updateDigits(timerItems[0], days, "Days");
+		updateDigits(timerItems[1], hours, "Hours");
+		updateDigits(timerItems[2], minutes, "Minutes");
+		updateDigits(timerItems[3], seconds, "Seconds");
 			
 	};
 	
